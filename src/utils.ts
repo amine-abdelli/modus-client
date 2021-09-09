9256c80eeb308aa50f613c28608e176c0d13caca
amine-abdelli <amine.abdelli@outlook.fr>
2021-09-09 15:46:13 +0200

    Create functions to format dates

--- src/utils.ts
+++ src/utils.ts
@@ -2,4 +2,10 @@ function formatDate(date: Date): string {
   return new Intl.DateTimeFormat('fr-FR').format(new Date(date));
 }
 
-export { formatDate };
+function formatDateToCalendar(date: Date): string {
+  const dateString = new Date(date);
+  dateString.setDate(dateString.getDate());
+  return `${dateString.getFullYear()}-${(`0${dateString.getMonth() + 1}`).slice(-2)}-${(`0${dateString.getDate()}`).slice(-2)}`;
+}
+
+export { formatDate, formatDateToCalendar };
