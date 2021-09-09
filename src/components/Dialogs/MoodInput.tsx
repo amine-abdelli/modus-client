eccdfb3063ea23a6634bc40a8c788926e98e56ff
amine-abdelli <amine.abdelli@outlook.fr>
2021-09-09 15:48:02 +0200

    feat(mood-dialog): Create component allowing user to choose a mood

--- src/components/Dialogs/MoodInput.tsx
+++ src/components/Dialogs/MoodInput.tsx
@@ -1,22 +1,35 @@
 import { Button } from 'antd';
+import { useState } from 'react';
 import { Mood } from './Mood.enum';
+import {
+  IconGenerator,
+} from './IconsGenerator';
+import './Icon.less';
 
 const MoodInput = ({ onMoodRateSelect }: any) => {
-  const moodArray = Object.values(Mood).slice(0, 5).map((moomood, i) => {
-    console.log();
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const moodArray = Object.values(Mood).slice(0, 5).map((mood, i) => {
+    const isNotSelected = activeIndex !== null && activeIndex !== i;
     return (
-      <Button
-        key={moomood}
-        onClick={() => onMoodRateSelect(i + 1)}
-      >
-        <img className="mood-logo" src={`${moomood}`} alt="Logo" />
-      </Button>
+      <>
+        <Button
+          key={mood}
+          disabled={!!isNotSelected}
+          onClick={() => {
+            onMoodRateSelect(i + 1);
+            setActiveIndex(i);
+          }}
+          type="link"
+        >
+          {IconGenerator(`${mood}`, (isNotSelected ? 'selectable' : ''))}
+        </Button>
+      </>
     );
   });
   return (
-    <>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
       {moodArray}
-    </>
+    </div>
   );
 };
 
