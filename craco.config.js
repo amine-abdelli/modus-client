168f13e5a156bf59df4133ef5fd96bfbd5bffa1c
amine-abdelli <amine.abdelli@outlook.fr>
2021-09-15 08:54:42 +0200

    feat(craco): Modify antd theme config file

--- craco.config.js
+++ craco.config.js
@@ -20,6 +20,7 @@ module.exports = {
               '@dark-grey-color': theme.darkGreyColor,
               '@light-grey-color': theme.lightGreyColor,
               '@light-background-color': theme.lightBackgroundColor,
+              '@layout-body-background': 'white',
               '@border-radius-base': theme.borderRadiusBase,
               '@padding-xss': theme.paddingXss,
               '@padding-xs': theme.paddingXs,
